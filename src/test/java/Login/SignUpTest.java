4c1a74d8b5779e84c3230cf36ba30301136591b7
Magda Nowakowska <magdanowakowska0@gmail.com>
2022-05-22 13:29:31 +0200

    Add sign up empty form test

--- src/test/java/Login/SignUpTest.java
+++ src/test/java/Login/SignUpTest.java
@@ -7,8 +7,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class SignUpTest {
 
@@ -78,6 +81,49 @@ public class SignUpTest {
 //        Assert.assertTrue(heading.getText().contains(lastName));
 //        Assert.assertTrue(heading.getText().contains(firstName));
 //        Assert.assertEquals(heading.getText(),"Hi, Jan Kowalski");
+    }
+    @Test
+    public void signUpEmptyForce() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+
+        String firstName = "Jan";
+        String lastName = "Kowalski";
+        int randomNumber = (int) (Math.random()*1000);
+        String email = "jankowalski" + randomNumber + "@gmial.com";
+        driver.findElements(By.xpath("//li[@id='li_myaccount']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()= '  Sign Up']")).get(1).click();
+        driver.findElement(By.xpath("//button[@type='submit' and text()=' Sign Up']")).click();
+        List<String> errors =
+                driver.findElements(By.xpath("//div[@class='alert alert-danger']//p"))
+                        .stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(errors.contains("The Email field is required."));
+        softAssert.assertTrue(errors.contains("The Password field is required."));
+        softAssert.assertTrue(errors.contains("The Password field is required."));
+        softAssert.assertTrue(errors.contains("The First name field is required."));
+        softAssert.assertTrue(errors.contains("The Last Name field is required."));
+        softAssert.assertAll();
+
+
+        //Assert.assertEquals("The Email field is required.","The Email field is required.");
+        //Assert.assertEquals("The Password field is required.","The Password field is required.");
+        //Assert.assertEquals("The Password field is required.","The Password field is required.");
+        //Assert.assertEquals("The First name field is required.","The First name field is required.");
+        //Assert.assertEquals("The Last Name field is required.","The Last Name field is required.");
+
     }
 }
 
